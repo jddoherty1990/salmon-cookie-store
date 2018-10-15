d906d402f456fd04cfe734fc37788fec68034296
James Dean Doherty <jddoherty1990@icloud.com>
2018-10-15 16:52:44 -0700

    restructured 1st and Pike store object with Noah's help

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 'use strict';
 
-var hours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+// 1st and Pike store (object literal)
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var firstAndPike = {
-    minCustPerHr: [23],
-    maxCustPerHr: [65],
-    avgCookiesPerCust: [6.3],  
+  minCustPerHr: 23,
+  maxCustPerHr: 65,
+  avgCookiesPerCust: 6.3,
+  getCustomersPerHour: function(min, max) {
+      // for the length of hours
+      // get random number between min and max
+      // push vls to customersPerHourArr
+  },
+  customersPerHour: [],
+  getCookiesPerHour: function() {
+      //for the length of customers per hour
+      // push into cookies per hour
+          // customersPerHour[i] * cookieperHour
+  },
+  cookiePerHour: []
+
+
 };
+// generate customer per hour function
+function genCustomerPerHr(minCustPerHr, maxCustPerHr) {
+  return Math.random() * (maxCustPerHr - minCustPerHr) + minCustPerHr;
+}
+// extract average cookies purchased per hour and store in 1st and Pike object
 
-// customerPerHr = function() {}
 
-function customerPerHr(minCustPerHr, maxCustPerHr) {
-    return Math.random() * (maxCustPerHr - minCustPerHr) + minCustPerHr;
-  }
+
+
+
+
+
+
+// console.log(genCustomerPerHr.firstAndPike.avgCookiesPerCust);
+
